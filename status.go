975ee41990a74342df268df49e32f125db3e014e
Illya Konovalov <dev@expertpal.io>
2023-06-05 20:15:14 +0200

    allow to mark messages as read

--- status.go
+++ status.go
@@ -41,6 +41,33 @@ func (obj *Status) MakeQr(phoneId, message, format string) (*StatusResponse, err
 
 }
 
+// MarkMessageAsRead marks a message as read
+func (obj *Status) MarkMessageAsRead(phoneId string) (*StatusResponse, error) {
+
+	endpoint := fmt.Sprintf("/%s/messages", phoneId)
+
+	body := make(map[string]interface{})
+	body["status"] = "read"
+	body["messaging_product"] = "whatsapp"
+	body["message_id"] = obj.MessageId
+
+	res, status, err := obj.api.request(endpoint, "POST", nil, body)
+	if err != nil {
+
+		return nil, err
+	}
+
+	if status != 200 {
+		e := ErrorResponse{}
+		json.Unmarshal(res, &e)
+		return nil, &e
+	}
+
+	var response StatusResponse
+	json.Unmarshal(res, &response)
+	return &response, nil
+}
+
 type StatusResponse struct {
 	Success bool `json:"success"`
 }
